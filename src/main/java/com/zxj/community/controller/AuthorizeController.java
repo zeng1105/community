2f1608f0a253f09bfad213a4f9a617472bdcfaac
zxj <312945094@qq.com>
2020-06-29 15:49:36 +0800

    完善登录跳转，添加session

--- src/main/java/com/zxj/community/controller/AuthorizeController.java
+++ src/main/java/com/zxj/community/controller/AuthorizeController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class AuthorizeController {
 
@@ -27,7 +29,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String Callback(@RequestParam(name="code") String code,
-                           @RequestParam(name="state")String state){
+                           @RequestParam(name="state")String state,
+                           HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -36,7 +39,14 @@ public class AuthorizeController {
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        //登录判断
+        if(user!=null){
+            //登录成功,将用户存入Session中
+            request.getSession().setAttribute("user",user);
+            return "redirect:/";//重定向回到首页。（不请求转发防止地址信息暴露）
+        } else {
+            //登录失败
+            return "redirect:/";
+        }
     }
 }
